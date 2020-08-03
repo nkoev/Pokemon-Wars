75874bf2c081956983be8c382da0cc023611a82b
nkoev <koev.niki@gmail.com>
2020-08-03 10:41:04 +0300

    'paths'

--- scripts/common.js
+++ scripts/common.js
@@ -1,5 +1,5 @@
 export const pokemonsContainer = document.getElementById("pokemons-container");
-export const backgroundUrl = "../../assets/images/background.jpg";
+export const backgroundUrl = "./assets/images/background.jpg";
 export const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
-export const music = new Audio("../../assets/sounds/battle.mp3");
-export const hitSound = new Audio("../../assets/sounds/hit.wav");
+export const music = new Audio("./assets/sounds/battle.mp3");
+export const hitSound = new Audio("./assets/sounds/hit.wav");
